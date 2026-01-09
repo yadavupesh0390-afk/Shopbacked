51d01b603ad372baf4006a653e7067c11451e5ad
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-01-10 00:10:10 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());              // ✅ MUST
-app.use(express.urlencoded({ extended: true }));
+
 
 
 /* ================== SCHEMA ================== */
