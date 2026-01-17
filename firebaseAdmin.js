b20235159e6e0da4a557961bfc00898c1336fecc
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-01-17 12:05:10 +0530

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,8 +1,8 @@
-import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
-export default admin;
+module.exports = admin;
