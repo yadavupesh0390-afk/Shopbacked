b999660f9b6e535056e064679d0166da2ec9b38e
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-01-17 23:35:16 +0530

    Update payments.js

--- routes/payments.js
+++ routes/payments.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Order = require("../models/Order");            // ✅ correct path
+const orderRoutes = require("./order");            // ✅ correct path
               // fcmToken yahin se aayega
 const admin = require("./firebaseAdmin");           // firebase admin
 
