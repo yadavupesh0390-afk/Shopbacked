7dbda2125b2208b710cf510afc5fac5ea47e722f
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-01-17 20:47:53 +0530

    Create sendNotification.js

---
+++ utils/sendNotification.js
@@ -0,0 +1,21 @@
+import admin from "../firebase.js";
+
+export async function sendNotification(token, title, body, data = {}) {
+  if (!token) return;
+
+  const message = {
+    token,
+    notification: {
+      title,
+      body
+    },
+    data
+  };
+
+  try {
+    await admin.messaging().send(message);
+    console.log("✅ Notification sent");
+  } catch (err) {
+    console.error("❌ Notification error", err.message);
+  }
+}
