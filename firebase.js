b3ef98d7077128dc5cfdb3d2b0d38a17cd89eaac
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-01-18 19:14:50 +0530

    Delete firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-export default admin;
