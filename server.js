bced8625adce8cc9363ec5e710439b63335217a0
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-01-19 12:41:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,15 +12,7 @@ app.use(cors());
 /* ================= MIDDLEWARE ================= */
 
 
-const admin = require("firebase-admin");
-if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-  throw new Error("❌ FIREBASE_SERVICE_ACCOUNT environment variable missing!");
-}
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+const admin = require("./firebaseAdmin");
 
 
 
