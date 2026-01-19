45944e853ac7e2e2b7f9b358824d6e5c16eb1172
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-01-19 12:42:43 +0530

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,18 +1,17 @@
-// firebaseAdmin.js
 const admin = require("firebase-admin");
 
-// ENV variable me JSON string ke roop me service account rakha hai
-// Isliye parse karna zaruri hai
 if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-  throw new Error("❌ FIREBASE_SERVICE_ACCOUNT environment variable missing!");
+  throw new Error("❌ FIREBASE_SERVICE_ACCOUNT env variable missing");
 }
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-console.log("✅ Firebase Admin initialized successfully");
+console.log("✅ Firebase Admin Initialized");
 
 module.exports = admin;
