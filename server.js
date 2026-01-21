121dc32cfeb1628843e5d2fb7169c256c0a14b99
yadavupesh0390-afk <yadavupesh0390@gmail.com>
2026-01-21 21:32:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -380,7 +380,20 @@ if (order.wholesalerLocation?.lat && order.wholesalerLocation?.lng) {
 
 const axios = require("axios");
 app.use(express.json({ limit: "10mb" }));
+function getDistanceKm(lat1, lon1, lat2, lon2) {
+  const R = 6371; // Earth radius in KM
+  const dLat = (lat2 - lat1) * Math.PI / 180;
+  const dLon = (lon2 - lon1) * Math.PI / 180;
 
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1 * Math.PI / 180) *
+    Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
+}
 
 // distance calculation in KM
   function isWithin20Km(loc1, loc2) {
